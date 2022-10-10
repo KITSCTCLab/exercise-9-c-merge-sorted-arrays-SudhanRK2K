3e2400ab5fab89528bb019ad248917ad56ad4910
SudhanRK2K <110010355+SudhanRK2K@users.noreply.github.com>
2022-10-10 21:07:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,11 @@ from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
  
+  x = nums1[0:m]
+  y = nums2[0:n]
+  nums1=x+y
+  nums1 = merge_sort(nums1)
+  return nums1 
   
 def merge_sort(myList) -> None:
   if len(myList) > 1:
@@ -10,8 +15,7 @@ def merge_sort(myList) -> None:
         right = myList[mid:]
 
         # Recursive call on each half
-        merge_sort(left)
-        merge_sort(right)
+
 
         # Two iterators for traversing the two halves
         i = 0
